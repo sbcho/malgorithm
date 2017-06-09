9832fb58bf98a2ea9fdb50276875dee39415e6b3
sb.cho <sb.cho@lge.com>
2017-06-09 09:29:53 +0900

    update 002_add_two_numbers reduing while loop

--- leecode/002_add_two_numbers/002_add_two_numbers.java
+++ leecode/002_add_two_numbers/002_add_two_numbers.java
@@ -1,38 +1,24 @@
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode init, tail, head;
-        head = tail = init= new ListNode(l1.val + l2.val);
+        head = tail = init = new ListNode(l1.val + l2.val);
         int carry = 0;
         if(head.val >= 10){
             carry = 1;
             head.val %= 10;
         }
-        while(l1.next != null && l2.next != null){
-            l1 = l1.next;
-            l2 = l2.next;
+        while(l1.next != null || l2.next != null){
+            if (l1.next != null){
+                l1 = l1.next;
+            } else {
+                l1.val = 0;
+            }
+            if (l2.next != null){
+                l2 = l2.next;
+            } else {
+                l2.val = 0;
+            }
             head = new ListNode(l1.val + l2.val + carry);
-            tail.next = head;
-            tail = head;
-            carry = 0;
-            if(head.val >= 10){
-                carry = 1;
-                head.val %= 10;
-            }
-        }
-        while(l1.next != null){
-            l1 = l1.next;
-            head = new ListNode(l1.val + carry);
-            tail.next = head;
-            tail = head;
-            carry = 0;
-            if(head.val >= 10){
-                carry = 1;
-                head.val %= 10;
-            }
-        } 
-        while(l2.next != null){
-            l2 = l2.next;
-            head = new ListNode(l2.val + carry);
             tail.next = head;
             tail = head;
             carry = 0;
@@ -48,8 +34,7 @@ public class Solution {
                 tail.next = head;
                 tail = head;
             }
-        }
-        
+        }  
         return init;
     }
 }
