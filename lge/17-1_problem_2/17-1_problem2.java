f3ce6458bee2deee7b45223ad7e2bc590f329224
sb.cho <sb.cho@lge.com>
2017-06-15 17:03:46 +0900

    update lge 17-2_problem2 imcomplete solution

--- lge/17-1_problem_2/17-1_problem2.java
+++ lge/17-1_problem_2/17-1_problem2.java
@@ -8,59 +8,44 @@ class HamiltonianCycle
     private int[]   path;
     private int[][] graph;
  
-    /** Function to find cycle 
-     * @return **/
     public boolean findHamiltonianCycle(int[][] g)
     {
         V = g.length;
         path = new int[V];
         Arrays.fill(path, -1);
         graph = g;
-        try
-        {
+        try{
             path[0] = 0;
             pathCount = 1;
-            solve(0);
+            next(0);
             return false;
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             return true;
         }
     }
  
-    /** function to find paths recursively **/
-    public void solve(int vertex) throws Exception
+    public void next(int vertex) throws Exception
     {
-        /** solution **/
         if (graph[vertex][0] == 1 && pathCount == V)
             throw new Exception("Solution found");
-        /** all vertices selected but last vertex not linked to 0 **/
         if (pathCount == V)
             return;
         for (int v = 0; v < V; v++)
         {
-            /** if connected **/
             if (graph[vertex][v] == 1)
             {
-                /** add to path **/
                 path[pathCount++] = v;
-                /** remove connection **/
                 graph[vertex][v] = 0;
                 graph[v][vertex] = 0;
-                /** if vertex not already selected solve recursively **/
                 if (!isPresent(v))
-                    solve(v);
-                /** restore connection **/
+                    next(v);
                 graph[vertex][v] = 1;
                 graph[v][vertex] = 1;
-                /** remove path **/
                 path[--pathCount] = -1;
             }
         }
     }
- 
-    /** function to check if path is already selected **/
     public boolean isPresent(int v)
     {
         for (int i = 0; i < pathCount - 1; i++)
@@ -68,24 +53,15 @@ class HamiltonianCycle
                 return true;
         return false;
     }
- 
-    /** display solution **/
-    public void display()
-    {
-        System.out.print("\nPath : ");
-        for (int i = 0; i <= V; i++)
-            System.out.print(path[i % V] + " ");
-        System.out.println();
-    }
 }
 public class Main {
-	static int point[];
-	static ArrayList<Integer> relation[];
+    static int point[];
+    static ArrayList<Integer> relation[];
     
-	@SuppressWarnings("resource")
-	public static void main(String[] args) throws IOException {
-    	FileInputStream in = null;
-    	FileOutputStream out = null;
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws IOException {
+        FileInputStream in = null;
+        FileOutputStream out = null;
         in = new FileInputStream("src/problem_1_Set0.in");
         out = new FileOutputStream("src/problem_1_Set0.out");
         InputStreamReader isr = new InputStreamReader(in);
@@ -97,151 +73,40 @@ public class Main {
         
         
         while(cases-- > 0) {
-        	int n = Integer.parseInt(br.readLine());
-        	point = new int[n];
-        	boolean visited[] = new boolean[n]; 
-        	relation = new ArrayList[n];
-        	for(int i = 0; i<n ; i++){
-        		point[i] = Integer.parseInt(br.readLine());
-        		relation[i] = new ArrayList<Integer>();
-        		visited[i] = false;
-        	}
-        	HamiltonianCycle hc = new HamiltonianCycle();
-        	int[][] graph = new int[n][n];
-
-        	System.out.println("cases:" + cases + ", n:" + n);
-        	
-        	long r;
-        	boolean hamilton = true;
-        	boolean result = false;
-
-        	for(r = 0; r < 10000; r++){
-        	//r=173;
-        		hamilton = true;
-        		result = true;
-//        		for(int i = 0; i < n; i++){
-//        			relation[i].clear();
-//        			for(int j = i; j < n; j++){
-//        				relation[j].clear();
-//        			}
-//        		}
-        		for(int i = 0; i < n; i++){
-        			for(int j = i+1; j < n; j++){
-//        				if(relation[i].contains(j)){
-//        					continue;
-//        				}
-        				if(point[i] >= point[j]){
-        					if(point[i] - r <= point[j] + r && point[i] - r >= point[j] - r){
-//        						relation[i].add(j);
-//        						relation[j].add(i);
-        						graph[i][j] = 1;
-        						graph[j][i] = 1;
-        						
-        					} 
-        				} else {
-        					if(point[i] + r >= point[j] - r && point[i] + r <= point[j] + r){
-//        						relation[i].add(j);
-//        						relation[j].add(i);
-        						graph[i][j] = 1;
-        						graph[j][i] = 1;
-        					}
-        				}
-        			}
-        		}
-//        		for(int i = 0; i < n; i++){
-//        			for(int j = i; j < n; j++){
-//        				if(relation[i].size() + relation[j].size() < n/2){
-//        					hamilton = false;
-//        					break;
-//        				}
-//        			}
-//        		}
-//        		if(!hamilton){
-//        			System.out.println("no hamilton : "+r);
-//        			//continue;
-//        		} else {
-//        			System.out.println("hamilton : "+r);
-//        			
-//        		}
-        		System.out.println(r);
-        		if(hc.findHamiltonianCycle(graph)){
-        			//result = false;
-        			break;
-        		}
-//        		for(int i = 0; i < n; i++){
-//        			if(relation[i].size()<2){
-//        				result = false;
-//        				break;
-//        			}
-//        		}
-//        		if(result){
-//        			break;
-//        		}
-//        		if(checkTour(r, n, point, relation, visited)){
-//        			System.out.println("tour");
-//        			break; 
-//        		}
-        		
-        	}
-        	if(n==1) r=0;
-        	System.out.println(r);
+            int n = Integer.parseInt(br.readLine());
+            point = new int[n];
+            for(int i = 0; i<n ; i++){
+                point[i] = Integer.parseInt(br.readLine());
+            }
+            HamiltonianCycle hc = new HamiltonianCycle();
+            int[][] graph = new int[n][n];
+            
+            long r;
+            for(r = 0; r < 100; r++){
+                for(int i = 0; i < n; i++){
+                    for(int j = i+1; j < n; j++){
+                        if(point[i] >= point[j]){
+                            if(point[i] - r <= point[j] + r && point[i] - r >= point[j] - r){
+                                graph[i][j] = 1;
+                                graph[j][i] = 1;
+                            } 
+                        } else {
+                            if(point[i] + r >= point[j] - r && point[i] + r <= point[j] + r){
+                                graph[i][j] = 1;
+                                graph[j][i] = 1;
+                            }
+                        }
+                    }
+                }
+                if(hc.findHamiltonianCycle(graph)){
+                    break;
+                }
+            }
+            if(n==1) r=0;
+            System.out.println("min:" + r);
             bw.write(String.valueOf(r));
             bw.newLine();
         }
         bw.close();
     }
-
-	private static boolean checkTour(long r, int n, int[] point, ArrayList<Integer>[] relation, boolean[] visited) {
-		// TODO Auto-generated method stub
-		boolean bTour = false;
-		int current = 0;
-
-
-
-		for(int i=0; i < relation[current].size(); i++){
-			//System.out.println("checkTour - current:" + current + ", relation[current].get(i):" + relation[current].get(i));
-			if(tour(current, relation[current].get(i), relation, visited)){
-				return true;
-			};
-		}
-		
-		return bTour;
-	}
-
-	private static boolean tour(Integer previous, Integer current, ArrayList<Integer>[] relation, boolean[] visited) {
-		// TODO Auto-generated method stub
-
-		visited[current] = true;
-		boolean bAllVisited = true;		
-		for(int i = 1; i < visited.length; i++){
-			if(visited[i] == false){
-				bAllVisited = false;
-				break;
-			}
-		}
-		if(bAllVisited && !relation[current].isEmpty() && relation[current].get(0) == 0){
-			return true;
-		}
-		
-		int indexOfCurrent = relation[previous].indexOf(current);
-		int indexOfPrevious = relation[current].indexOf(previous);
-		relation[previous].remove(current);
-		relation[current].remove(previous);
-		
-		for(int i = 0; i < relation[current].size(); i++){
-			if(visited[relation[current].get(i)] == true){
-				continue;
-			}
-			//System.out.println("check - current:" + current + ", relation[current].get(i):" + relation[current].get(i));
-			if(tour(current, relation[current].get(i), relation, visited)){
-				return true;
-			}
-		}
-		relation[previous].add(indexOfCurrent, current);
-		relation[current].add(indexOfPrevious, previous);		
-		visited[current] = false;
-		
-		return false;
-	}
-
 }
