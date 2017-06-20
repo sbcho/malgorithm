490688e1f8a3037af21687f079777bdb74e8ffee
craxy2010 <craxy2010@gmail.com>
2017-06-20 15:27:37 +0900

    Update 2163_divide_chocolate to correct solution

--- baekjoon/2163_divide_chocolate/2163_divide_chocolate.java
+++ baekjoon/2163_divide_chocolate/2163_divide_chocolate.java
@@ -11,15 +11,22 @@ public class Main
 		int n = scan.nextInt();
 		int m = scan.nextInt();
 		
-		memo = new int [300][300];
+		memo = new int [n+1][m+1];
+		for(int i = 0; i <= n; i++){
+			for(int j = 0; j <= m; j++){
+				memo[i][j] = -1;
+			}
+		}
+		memo[1][1] = 0;
+		if(m > 1) memo[1][2] = 1;
+		if(n > 1) memo[2][1] = 1;
 		
-		System.out.println(solve(n,m));
+		int result = solve(n,m);
+		System.out.println(result);
 	}
 
 	private static int solve(int n, int m) {
-		if(memo[n][m] > 0) return memo[n][m];
-		if(n == 1 && m == 1) return 0;
-		if(n == 2 && m == 1 || n == 1 && m == 2) return 1;
+		if(memo[n][m] >= 0) return memo[n][m];
 		
 		int min = Integer.MAX_VALUE;
 		
@@ -29,7 +36,7 @@ public class Main
 		for(int j = 1; j < m; j++){
 			min = Math.min(min, solve(n, j) + solve(n, m-j));
 		}
-		memo[n][m] = min;
+		memo[n][m] = min+1;
 		
 		return min+1;
 	}
