ceec80bdd63b118e73d85d235bae079a495e9724
craxy2010 <craxy2010@gmail.com>
2017-06-24 12:48:49 +0900

    Update 11052_selling_fish_bun.java

--- baekjoon/11052_selling_fish_bun/11052_selling_fish_bun.java
+++ baekjoon/11052_selling_fish_bun/11052_selling_fish_bun.java
@@ -7,18 +7,17 @@ public class Main{
 
 	public static void main(String[] args){
     	
-    	Scanner scan = new Scanner(System.in);
-    	int n = scan.nextInt();
+    		Scanner scan = new Scanner(System.in);
+    		int n = scan.nextInt();
     	
-    	price = new int[n];
-    	memo = new int[n];
+    		price = new int[n];
+    		memo = new int[n];
     	
-    	for(int i=0; i<n; i++){
-    		price[i] = scan.nextInt();
-    		memo[i] = -1;
-    	}
-    	
-    	System.out.println(solve(n));
+    		for(int i=0; i<n; i++){
+    			price[i] = scan.nextInt();
+    			memo[i] = -1;
+    		}
+    		System.out.println(solve(n));
 	}
 	
 	private static int solve(int n){
@@ -27,8 +26,7 @@ public class Main{
         
 		for(int i = n; i > 0; i--){
 			memo[n-1] = Math.max(memo[n-1], price[i-1] + solve(n-i));
-        }
-	    
-        return memo[n-1];
+        	}	    
+        	return memo[n-1];
 	}
 }
