c200287f84dfa164baede4957dbbfdf42736ae3f
craxy2010 <craxy2010@gmail.com>
2017-07-03 23:11:35 +0900

    Update 003_longest_substring_o(n2).java

--- leetcode/003_longest_substring/003_longest_substring_o(n2).java
+++ leetcode/003_longest_substring/003_longest_substring_o(n2).java
@@ -7,7 +7,7 @@ public class Solution {
         if(c.length == 1) return 1;
     	for(int k = 0; k < c.length; k++){
             for(int i = k; i < c.length; i++){
-				if(b[c[i]]) {
+		if(b[c[i]]) {
                     max = Math.max(max, i-k);
                     break;
                 }
