f279bff4e0e101f64304d7157cc639243705ebc2
craxy2010 <craxy2010@gmail.com>
2017-07-14 11:17:17 +0900

    Update 2493_top.java

--- baekjoon/2493_top/2493_top.java
+++ baekjoon/2493_top/2493_top.java
@@ -6,22 +6,22 @@ public class Main{
 	
 	public static void main(String[] args){
     	
-    	Scanner scan = new Scanner(System.in);
-    	
-    	int n = scan.nextInt();
-    	int[] top = new int[n];
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+		int[] top = new int[n];
 		int res;
-		
-    	for(int i = 0; i < n; i++){
-    		top[i] = scan.nextInt();
-    		res = 0;
-    		for(int j = i-1; j >=0; j--){
-    			if(top[j] >= top[i]){
-    				res = j + 1;
-    				break;			
-    			}
-    		}
-    		System.out.print(res + " ");
-    	}
+
+		for(int i = 0; i < n; i++){
+			top[i] = scan.nextInt();
+			res = 0;
+			for(int j = i-1; j >=0; j--){
+				if(top[j] >= top[i]){
+					res = j + 1;
+					break;			
+				}
+			}
+			System.out.print(res + " ");
+		}
 	}
 }
